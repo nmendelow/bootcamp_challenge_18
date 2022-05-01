a07f61c7103262669c696b6e7cd6236cfc1734a3
Neil Mendelow <nmendelow@hotmail.com>
2022-05-01 15:30:23 -0500

    updated readme to host the required screencaps showing the blockchain and validation from Streamlit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bootcamp_challenge_18
 A blockchain-based ledger system, complete with a user-friendly web interface
+
+screenshot of the blockchain
+
+![ledger](/Images/ledger.png)
+
+blochain validation
+
+![validaion](/Images/valid_chain.png)
--- pychain.py
+++ pychain.py
@@ -53,7 +53,7 @@ import hashlib
 
 # Add the Record Data class
 @dataclass
-class RecordData:
+class Record:
     sender: str
     receiver: str
     amount: float
@@ -74,7 +74,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    record: RecordData
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -195,7 +195,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=record,
+        record= Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
